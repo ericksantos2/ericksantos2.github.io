a98524384fff4c4bd9c5770db63e39a01f2bf4f8
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-02-01 16:49:12 -0300

    adicionei links novos no rodape (linkedin e github)

--- src/components/Rodape/index.jsx
+++ src/components/Rodape/index.jsx
@@ -1,9 +1,32 @@
 import styles from "./Rodape.module.scss";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Rodape() {
   return (
     <footer className={styles.rodape}>
-      <a href="https://github.com/ericksantos2/ericksantos2.github.io" target="_blank">Feito e projetado por Erick Santos</a>
+      <a
+        href="https://github.com/ericksantos2/ericksantos2.github.io"
+        target="_blank"
+        className={styles.rodape__texto}
+      >
+        Feito e projetado por Erick Santos
+      </a>
+      <div className={styles.rodape__links}>
+        <a
+          href="https://github.com/ericksantos2"
+          target="_blank"
+          className={styles.rodape__link}
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/erick-santos-16b54b252/"
+          target="_blank"
+          className={styles.rodape__link}
+        >
+          <FaLinkedin />
+        </a>
+      </div>
     </footer>
   );
 }
