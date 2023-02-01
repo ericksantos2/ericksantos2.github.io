0c0893a5fd5b90153304feec5c01151f00a1b3f8
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-02-01 16:46:02 -0300

    adicionei uma classe nos links e tambem adicionei um texto ao lado dos links

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -6,14 +6,21 @@ export default function Navbar() {
     <header className={styles.cabecalho}>
       <img id={styles.logo} src="img/logo.svg" alt="logo do site" />
       <div className={styles.links}>
-        <a href="https://github.com/ericksantos2" target="_blank">
+        <a
+          href="https://github.com/ericksantos2"
+          target="_blank"
+          className={styles.link}
+        >
           <FaGithub />
+          Github
         </a>
         <a
           href="https://www.linkedin.com/in/erick-santos-16b54b252/"
           target="_blank"
+          className={styles.link}
         >
           <FaLinkedin />
+          LinkedIn
         </a>
       </div>
     </header>
