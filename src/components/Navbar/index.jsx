3c1cf97a5167a7589900f40a074f92887071a69b
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-02-01 15:25:16 -0300

    removi um comentário e um 'a' que tinha no logo que não levava pra lugar nenhum

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,16 +4,8 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 export default function Navbar() {
   return (
     <header className={styles.cabecalho}>
-      <a href="#">
-        <img id={styles.logo} src="img/logo.svg" alt="logo do site" />
-      </a>
+      <img id={styles.logo} src="img/logo.svg" alt="logo do site" />
       <div className={styles.links}>
-        {/* <a href="#" className={styles["links__item--hover"]}>
-          Início
-        </a>
-        <a href="#sobremim" className={styles["links__item--hover"]}>
-          Sobre Mim
-        </a> */}
         <a href="https://github.com/ericksantos2" target="_blank">
           <FaGithub />
         </a>
