55e0be6079747d81e086bc62815358b94691bd30
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-02-01 16:48:03 -0300

    removi o build.target e adicionei o plugin novo

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), topLevelAwait()],
   resolve: {
     alias: [
       {
@@ -13,7 +14,4 @@ export default defineConfig({
       },
     ],
   },
-  build: {
-    target: "esnext"
-  }
 });
