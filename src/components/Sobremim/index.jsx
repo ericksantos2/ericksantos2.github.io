5f08cb2ffaf8ddfd34ff3e7f4d507af7a0242001
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-06-13 20:48:36 -0300

    agora o sobre mim e o email vem de uma chamada ao json do github

--- src/components/Sobremim/index.jsx
+++ src/components/Sobremim/index.jsx
@@ -3,16 +3,28 @@ import * as Icones from "react-icons/si";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+const sobreMimInitialState = {
+  textoSobremim: '',
+  emailContato: ''
+}
+
 export default function Sobremim() {
   const [habilidades, setHabilidades] = useState([]);
+  const [sobreMim, setSobreMim] = useState(sobreMimInitialState);
 
   async function conectaApiHabilidades() {
-    const resposta = await axios.get('https://raw.githubusercontent.com/ericksantos2/ericksantos2.github.io/master/src/components/Sobremim/habilidades.json');
-    setHabilidades(resposta.data);
+    const { data: resposta } = await axios.get('https://raw.githubusercontent.com/ericksantos2/ericksantos2.github.io/master/src/components/Sobremim/habilidades.json');
+    setHabilidades(resposta);
+  }
+
+  async function conectaApiSobreMim() {
+    const { data: resposta } = await axios.get('https://raw.githubusercontent.com/ericksantos2/ericksantos2.github.io/master/src/components/Sobremim/sobremim.json');
+    setSobreMim(resposta);
   }
 
   useEffect(() => {
     conectaApiHabilidades()
+    conectaApiSobreMim()
   }, [])
 
   return (
@@ -20,7 +32,7 @@ export default function Sobremim() {
       <div className={styles.sobremim__resumo}>
         <h1 className={styles.sobremim__resumo__titulo}>Sobre Mim</h1>
         <p className={styles.sobremim__resumo__texto}>
-          Desde que conheci o mundo da programação em agosto de 2022, me tornei um apaixonado por programação front-end e desde então venho sempre tentando melhorar as minhas habilidades em programação em busca da minha primeira oportunidade.
+          {sobreMim.textoSobremim}
         </p>
       </div>
       <div className={styles.sobremim__habilidades}>
@@ -45,7 +57,7 @@ export default function Sobremim() {
       <div className={styles.sobremim__contato}>
         <h1 className={styles.sobremim__contato__titulo}>Contato</h1>
         <p className={styles.sobremim__contato__email}>
-          Email: <a href="mailto:erick.s.g.barros@gmail.com">erick.s.g.barros@gmail.com</a>
+          Email: <a href={`mailto:${sobreMim.emailContato}`}>{sobreMim.emailContato}</a>
         </p>
       </div>
     </div>
